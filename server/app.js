a3a82b1d4a2903e51043130c3bd6237793e3bdc4
An <nguyenthanhan.binhminh@gmail.com>
2020-05-30 13:14:03 +0700

    2. What is middleware in nodejs?

--- server/app.js
+++ server/app.js
@@ -2,8 +2,19 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
+const customMiddleware = (req, res, next) => {
+    console.log("Middleware executed!");
+    next();
+}
+
 app.get('/', (req, res) => {
+    console.log("home");
     res.send("Hello world.");
+});
+
+app.get('/about', customMiddleware, (req, res) => {
+    console.log("about");
+    res.send("About page.");
 });
 
 app.listen(PORT, () => {
